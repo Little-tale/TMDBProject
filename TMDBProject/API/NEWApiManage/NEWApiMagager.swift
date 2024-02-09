c78ffb874abd687e5266a1a266037eb4877d1d25
judge_happily <you3192001@gmail.com>
2024-02-09 14:02:27 +0900

    Video API 통신 테스트 전 세팅완료

---
+++ TMDBProject/API/NEWApiManage/NEWApiMagager.swift
@@ -0,0 +1,114 @@
+//
+//  NEWApiMagager.swift
+//  TMDBProject
+//
+//  Created by Jae hyung Kim on 2/9/24.
+//
+
+import Foundation
+
+typealias naverUrl<T:Decodable> = (Result<T,Error>) -> Void
+
+typealias urlRequestTest<T:Decodable> = Result<T,NewAPIManagerRequestError>
+
+typealias urlTest = Result<URLRequest,NewAPIManagerRequestError>
+
+final class URLSessionNaver {
+    private init() {}
+    
+    static let shared =  URLSessionNaver()
+
+    func fetchSession<T: Decodable>(type: T.Type, API: UrlSession, completionHandler: @escaping naverUrl<T>) {
+    
+        let componentsTest = componentsErrorCatch(API:API)
+        switch componentsTest {
+        case .success(let url):
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                DispatchQueue.main.async {
+                    let result = self.urlErrorCatch(type:T.self, data: data, response: response, error: error)
+                    switch result {
+                    case .success(let success) :
+                        completionHandler(.success(success))
+                    case .failure(let failer) :
+                        completionHandler(.failure(failer))
+                    }
+                }
+            }.resume()
+        case .failure(let error) :
+            DispatchQueue.main.async {
+                completionHandler(.failure(error))
+            }
+        }
+        
+    }
+    
+    // MARK: 컴포넌트 전용 에러 캐치
+    private func componentsErrorCatch(API:UrlSession) -> urlTest{
+        var urlComponents = URLComponents()
+        urlComponents.scheme = API.schem
+        urlComponents.host = API.host
+        urlComponents.path = "/" + API.path
+        if let apikeys = API.query {
+            urlComponents.queryItems = [apikeys]
+        }
+        
+        // urlComponents.queryItems = API.header
+        
+        // print(urlComponents.queryItems)
+        
+        guard let url = urlComponents.url else {
+            print("url 컴포넌츠 미스")
+            return .failure(.componatsError)
+        }
+        
+        var urlRe = URLRequest(url: url)
+        print(urlRe.headers)
+        urlRe.allHTTPHeaderFields = API.header
+        
+        // print(urlRe.headers)
+        print(urlRe.url)
+        return .success(urlRe)
+    }
+    
+    
+    // MARK: URL 테스트 전용 에러 캐치
+    private func urlErrorCatch<T:Decodable>(type:T.Type, data: Data?, response: URLResponse?, error: Error?) -> urlRequestTest<T> {
+        guard error == nil else {
+            print("에러가 존재 즉 요청실패입니다.")
+            return urlRequestTest.failure(.failRequest)
+        }
+        guard let data = data else {
+            print("에러는 없으나 데이터가 없습니다.")
+            return urlRequestTest.failure(.nodata)
+        }
+        print(data)
+        guard let response = response else {
+            print("에러는 없으나 응답이 오지 않았습니다.")
+            return urlRequestTest.failure(.noResponse)
+        }
+        print(response)
+        guard let response = response as? HTTPURLResponse else {
+            print("응답은 있었으나 응답코드로 변경을 싪패")
+            return urlRequestTest.failure(.errorResponse)
+        }
+        print(response.statusCode)
+        
+        guard response.statusCode == 200 else {
+            print("응답코드 200이 아닌 상황 발생")
+            if response.statusCode == 400{
+                print("정상적인 쿼리가 아님")
+            }
+            return urlRequestTest.failure(.cantStatusCoding)
+        }
+        
+        do{
+            let result = try JSONDecoder().decode(T.self, from: data)
+            return urlRequestTest.success(result)
+        } catch {
+            print("디코딩에 실패 하였습니다.")
+            return urlRequestTest.failure(.errorDecoding)
+        }
+        
+    }
+    
+}
--- TMDBProject/API/NaverAPI.swift
+++ TMDBProject/API/NaverAPI.swift
@@ -7,11 +7,10 @@
 
 import Foundation
 
-//protocol URLRequest{
-//    
-//}
 
-enum NaverError: Error{
+
+
+enum NewAPIManagerRequestError: Error{
     case nodata
     case noResponse
     case errorResponse
@@ -22,9 +21,13 @@ enum NaverError: Error{
     case componatsError
 }
 
+/*
+ URL Session 의 컴포넌트 방식으로 하지만
+ BaseUrl를 구현하라고 한 이유는 만일의 사태일때 그것으로 대체하라는 의미에 두었습니다.
+ */
 protocol UrlSession{
     var baseUrl : String {get}
-    var query : URLQueryItem {get}
+    var query : URLQueryItem? {get}
     var header : [String: String] {get}
     var method : String {get}
     
@@ -50,7 +53,7 @@ enum naverApi:UrlSession {
         }
     }
     
-    var query: URLQueryItem {
+    var query: URLQueryItem? {
         switch self {
         case .searchImage(searchText: let searchText,_):
             // let text = searchText.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed )
@@ -90,108 +93,70 @@ enum naverApi:UrlSession {
     
 }
 
-struct NaverComponets {
-    // let uiButton = UIButton(type: .plain, primaryAction : UIAction)
+enum NewTMDB: UrlSession {
+    case videos(key: Int)
+    /*schme: https
+     host: api.themoviedb.org
+     path: /3/tv/{series_id}/videos
+     qury: NULL*/
+    
+    var baseUrl: String {
+        switch self {
+        case .videos(let key):
+            return "https://api.themoviedb.org/3/tv/\(key)/videos"
+        }
+    }
+     // MARK: Base URL 을 쓰진 않지만 그렇다면 조립할수 있다면? -> 이것도 좋기는 한데.... 에러 처리가 애매해짐...
+//    var testBaseUrl: URL {
+//        switch self {
+//        case .videos(let key):
+//            var component = URLComponents()
+//            component.scheme = "https"
+//            component.host = "api.themoviedb.org"
+//            component.path = "3/tv/\(key)/videos"
+//            component.url
+//            
+//        }
+//    }
+    
+    var query: URLQueryItem? {
+        switch self {
+        case .videos:
+            return nil
+        }
+    }
+    
+    var header: [String : String] {
+        return TMDBAPIkey().getAPI
+    }
+    
+    var method: String {
+        return "GET"
+    }
+    
+    var schem: String {
+        switch self {
+        case .videos(let key):
+            return "https"
+        }
+    }
+    
+    var host: String {
+        switch self {
+        case .videos(let key):
+            "api.themoviedb.org"
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .videos(let key):
+            "/3/tv/\(key)/videos"
+        }
+    }
+    
+    
 }
 // 원래 있는걸 써도 좋지만 연습을 위해 계속 만듭니다.
-typealias naverUrl<T:Decodable> = (Result<T,NaverError>) -> Void
-typealias urlRequestTest<T:Decodable> = Result<T,NaverError>
-typealias urlTest = Result<URLRequest,NaverError>
 
-final class URLSessionNaver {
-    private init() {}
-    
-    static let shared =  URLSessionNaver()
-
-    func fetchSession<T: Decodable>(type: T.Type, API: UrlSession, completionHandler: @escaping naverUrl<T>) {
-    
-        let componentsTest = componentsErrorCatch(API:API)
-        switch componentsTest {
-        case .success(let url):
-            URLSession.shared.dataTask(with: url) { data, response, error in
-                DispatchQueue.main.async {
-                    let result = self.urlErrorCatch(type:T.self, data: data, response: response, error: error)
-                    switch result {
-                    case .success(let success) :
-                        completionHandler(.success(success))
-                    case .failure(let failer) :
-                        completionHandler(.failure(failer))
-                    }
-                }
-            }.resume()
-        case .failure(let error) :
-            DispatchQueue.main.async {
-                completionHandler(.failure(error))
-            }
-        }
-        
-    }
-    
-    // MARK: 컴포넌트 전용 에러 캐치
-    private func componentsErrorCatch(API:UrlSession) -> urlTest{
-        var urlComponents = URLComponents()
-        urlComponents.scheme = API.schem
-        urlComponents.host = API.host
-        urlComponents.path = "/" + API.path
-        urlComponents.queryItems = [API.query]
-        // urlComponents.queryItems = API.header
-        
-        // print(urlComponents.queryItems)
-        
-        guard let url = urlComponents.url else {
-            print("url 컴포넌츠 미스")
-            return .failure(.componatsError)
-        }
-        
-        var urlRe = URLRequest(url: url)
-        print(urlRe.headers)
-        urlRe.allHTTPHeaderFields = API.header
-        
-        // print(urlRe.headers)
-        print(urlRe.url)
-        return .success(urlRe)
-    }
-    
-    
-    // MARK: URL 테스트 전용 에러 캐치
-    private func urlErrorCatch<T:Decodable>(type:T.Type, data: Data?, response: URLResponse?, error: Error?) -> urlRequestTest<T> {
-        guard error == nil else {
-            print("에러가 존재 즉 요청실패입니다.")
-            return urlRequestTest.failure(.failRequest)
-        }
-        guard let data = data else {
-            print("에러는 없으나 데이터가 없습니다.")
-            return urlRequestTest.failure(.nodata)
-        }
-        print(data)
-        guard let response = response else {
-            print("에러는 없으나 응답이 오지 않았습니다.")
-            return urlRequestTest.failure(.noResponse)
-        }
-        print(response)
-        guard let response = response as? HTTPURLResponse else {
-            print("응답은 있었으나 응답코드로 변경을 싪패")
-            return urlRequestTest.failure(.errorResponse)
-        }
-        print(response.statusCode)
-        
-        guard response.statusCode == 200 else {
-            print("응답코드 200이 아닌 상황 발생")
-            if response.statusCode == 400{
-                print("정상적인 쿼리가 아님")
-            }
-            return urlRequestTest.failure(.cantStatusCoding)
-        }
-        
-        do{
-            let result = try JSONDecoder().decode(T.self, from: data)
-            return urlRequestTest.success(result)
-        } catch {
-            print("디코딩에 실패 하였습니다.")
-            return urlRequestTest.failure(.errorDecoding)
-        }
-        
-    }
-    
-}
 
